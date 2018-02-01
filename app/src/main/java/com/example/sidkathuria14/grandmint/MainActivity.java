7db72191bd0285c59d24c4ce4e5f88d632f7a2fb
siddharth kathuria <kathuria.siddharth@yahoo.in>
2018-02-01 18:36:51 +0530

    menu added

--- app/src/main/java/com/example/sidkathuria14/grandmint/MainActivity.java
+++ app/src/main/java/com/example/sidkathuria14/grandmint/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.sidkathuria14.grandmint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,6 +30,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import static android.R.attr.x;
+import static android.R.id.message;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -267,6 +272,30 @@ else Toast.makeText(MainActivity.this, "email not verified", Toast.LENGTH_SHORT)
             findViewById(R.id.signed_in_buttons).setVisibility(View.GONE);
         }
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
 
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+//        int id == item.getItemId();
+        switch (item.getItemId()) {
+
+            case R.id.sourceCode:
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://github.com/sidkathuria14/sample-firebase-authentication"));
+                startActivity(intent);
+                return true;
+            default: return super.onOptionsItemSelected(item);
+
+        }
+
+    }
 
 }
